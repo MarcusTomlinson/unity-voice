7c3ebdb3f6da695dafef4bdf153e9c78b7201d57
Pete Woods <pete.woods@canonical.com>
2013-08-23 12:40:35 +0100

    Include from common, instead of libunityvoice

--- src/service/AdaptorInclude.h
+++ src/service/AdaptorInclude.h
@@ -8,7 +8,7 @@
 #ifndef ADAPTORINCLUDE_H_
 #define ADAPTORINCLUDE_H_
 
-#include <libunityvoice/UnityVoice.h>
+#include <common/DBusTypes.h>
 #include <VoiceService.h>
 
 #endif /* ADAPTORINCLUDE_H_ */
